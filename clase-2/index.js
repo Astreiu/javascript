90acbeff6deaa864c4f20a063038af8bbb0c1acf
Matias Braso <carlosmatiasbraso@gmail.com>
2022-08-15 21:30:42 -0300

    clase-1 y clase-2

---
+++ clase-2/index.js
@@ -0,0 +1,108 @@
+/*
+= asignacion
+== comparacion que solo valida valor
+=== comparacion que valida tanto valor como tipo de dato
+*/
+
+/*
+    console.log(3 == "3");
+    console.log(3 === "3");
+*/
+
+/*
+estructura del if
+
+if(condicion){
+    codigo a ejecutar en caso que la condicion sea verdadera
+}
+*/
+
+/*
+
+ if (true){
+     console.log("entramos al if");
+ }
+
+ console.log("fin del proceso")
+
+ let nombre = "Diana";
+
+ if(nombre === "Diana"){
+     console.log("Hola Diana");
+ } 
+
+ console.log("chao Diana");
+
+ if("camila"){
+     console.log("entramos");
+ }
+
+ let numero = 19;
+
+ if(numero === 5){
+     console.log("Hola 5");
+ }
+
+ console.log("Intermedio");
+
+
+ if(numero === 6){
+     console.log("Hola 6");
+ }
+
+ console.log("Terminamos");
+
+estructura if else
+if(condicion){
+    condicion a ejecutar en caso de que la condicion sea verdadera
+}else{
+    condicion a 
+}
+
+ let numero = 10;
+
+ if(numero === 30){
+     console.log("eres igual a 30");
+ }else{
+     console.log("no eres 30");
+ }
+
+ console.log("terminamos");
+
+ let clima = "cae agua";
+
+ if(clima === "hace sol"){
+     console.log("salgo al parque");
+ }else{
+     console.log("no salgo al parque");
+ }
+
+ console.log("terminamos");
+
+ let respuesta = prompt("Terminaste la tarea?");
+
+ if(respuesta.toLowerCase() ==="si"){
+     alert("Puedes salir a jugar");
+ }else{
+     alert("No puedes salir a jugar");
+ }
+
+/*
+let edad = parseInt(prompt("Ingresa tu edad"));
+
+if(edad < 15){
+    alert("No puedes entrar");
+}else if(edad < 18){
+    alert("Puedes entrar con un adulto");
+}else{
+    alert("Puedes entrar");
+}
+
+alert("Gracias por ingresar tu edad");
+*/
+
+/*
+let numero = 5;
+let valor = true;
+let valorUno = numero === 10;
+*/
---
+++ desafio/index.js
@@ -0,0 +1,7 @@
+
+let saludo = "Hola"
+let nombre = prompt("Ingrese su nombre");
+let apellido = prompt("Ingrese apellido");
+let mensaje = `${saludo} ${nombre} ${apellido}`;
+
+alert(mensaje);
